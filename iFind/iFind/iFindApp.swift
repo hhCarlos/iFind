c4e199b231394521ba80d185dc075098f83989ab
hhCarlos <52683217+hhCarlos@users.noreply.github.com>
2023-05-11 17:14:48 -0600

    Add files via upload

---
+++ iFind/iFind/iFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFindApp.swift
+//  iFind
+//
+//  Created by Carlos Enrique Hernandez Hernandez on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
